2092ab6c5e55ab95a8c0d80ccb5e545e73e6b2d1
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2021-10-15 16:34:42 +0200

    github actions added

--- rusterella/scripts/test.sh
+++ rusterella/scripts/test.sh
@@ -5,6 +5,7 @@ export CARGO_INCREMENTAL=0
 export LLVM_PROFILE_FILE="target/prof/rusterella-%p-%m.profraw"
 # export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTFLAGS="-Zinstrument-coverage"
+export RUSTUP_TOOLCHAIN=nightly
 
 
 mkdir -p temp
