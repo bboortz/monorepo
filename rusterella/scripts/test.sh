baba53013216741656ab458edc6c7af9c7761c1e
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2021-10-15 15:38:53 +0200

    github actions added

--- rusterella/scripts/test.sh
+++ rusterella/scripts/test.sh
@@ -3,6 +3,7 @@
 export RUSTDOCFLAGS="-Cpanic=abort"
 export CARGO_INCREMENTAL=0
 export LLVM_PROFILE_FILE="target/prof/rusterella-%p-%m.profraw"
+# export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTFLAGS="-Zinstrument-coverage"
 
 
