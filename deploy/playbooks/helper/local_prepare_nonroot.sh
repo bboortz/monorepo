c140f98cd7af4639f76d12f8c68fd872c7541543
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2021-12-05 17:47:16 +0100

    python virtual env repaired

---
+++ deploy/playbooks/helper/local_prepare_nonroot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -d ".venv" ]; then
+    echo "INFO: creating .venv ..."
+    python3 -m venv .venv
+fi
+
+echo "INFO: installing/updating packages into .venv ..."
+source .venv/bin/activate
+pip3 install -U pip
+pip3 install -U -r requirements.txt
+ansible-galaxy install GROG.management-user
+deactivate
+
+
+echo "INFO: installing further ansible plugins ..."
+mkdir -p ~/.ansible/plugins/lookup && cd "$_"
+curl https://raw.githubusercontent.com/viczem/ansible-keepass/master/keepass.py -o ./keepass.py
+cd -
--- deploy/playbooks/helper/local_prepare_root.sh
+++ deploy/playbooks/helper/local_prepare_root.sh
@@ -4,19 +4,10 @@ source /etc/os-release
 
 
 which python3 || {
+  echo "INFO: installing necessary OS packages"
   if [ "$ID_LIKE" == "arch" ]; then
     sudo pacman -S make python3 python-pip
   else
     apt-get install git make python3 python3-venv python3-pip
   fi
 }
-
-if [ ! -d ".venv" ]; then
-  python3 -m venv .venv
-fi
-
-source .venv/bin/activate
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
-ansible-galaxy install GROG.management-user
-deactivate
