496e23e9497252036950898965232d23d95bccff
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2022-01-03 15:43:16 +0100

    test cases added

--- relf/src/display.rs
+++ relf/src/display.rs
@@ -30,3 +30,31 @@ fn to_hex(data: &[u8]) -> String {
     (&hex[8..]).to_string()
     */
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_print_field() {
+        let data: Vec<u8> = vec![
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05,
+            0x06, 0x07, 0x03, 0x3E,
+        ];
+        let expected = String::from("[127, 69, 76, 70, 1, 1, 1, 3, 1, 1, 2, 3, 4, 5, 6, 7, 3, 62]");
+        //let result = print_field(f, "KEY", "VALUE", &data);
+        let result = format!("{:?}", data);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_tohex() {
+        let data: Vec<u8> = vec![
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05,
+            0x06, 0x07, 0x03, 0x3E,
+        ];
+        let expected = String::from("00000000  7F 45 4C 46 01 01 01 03 01 01 02 03 04 05 06 07  | ⌂ELF☺☺☺♥☺☺☻♥♦♣♠• |\n00000010  03 3E                                            | ♥>               |");
+        let result = to_hex(&data);
+        assert_eq!(expected, result);
+    }
+}
