9c92e46a6372e217e46bc9b027cce5324598d2b6
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2022-01-08 10:55:27 +0100

    elf headertype and headermachine fixed

--- relf/src/elf.rs
+++ relf/src/elf.rs
@@ -212,36 +212,28 @@ impl fmt::Display for FileHeaderIdent {
 }
 
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
-#[deku()]
-pub struct FileHeader {
-    ident: FileHeaderIdent,
-    r#type: HeaderType,
-    machine: HeaderMachine,
-}
-
-#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
-#[deku(type = "u8")]
+#[deku(type = "u16", bytes = "2", endian = "little")]
 pub enum HeaderType {
-    #[deku(id = "0x00")]
+    #[deku(id = "0x0000")]
     None,
-    #[deku(id = "0x01")]
+    #[deku(id = "0x0001")]
     Rel,
-    #[deku(id = "0x02")]
+    #[deku(id = "0x0002")]
     Exec,
-    #[deku(id = "0x03")]
+    #[deku(id = "0x0003")]
     Dyn,
-    #[deku(id = "0x04")]
+    #[deku(id = "0x0004")]
     Core,
 }
 
 impl fmt::Display for HeaderType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let (data_str, byte): (&str, u8) = match *self {
-            HeaderType::None => ("No file type", 0x00),
-            HeaderType::Rel => ("Relocatable file", 0x01),
-            HeaderType::Exec => ("Executable file", 0x02),
-            HeaderType::Dyn => ("Shared object file", 0x03),
-            HeaderType::Core => ("Core file", 0x04),
+        let (data_str, byte): (&str, u16) = match *self {
+            HeaderType::None => ("No file type", 0x0000),
+            HeaderType::Rel => ("Relocatable file", 0x0100),
+            HeaderType::Exec => ("Executable file", 0x0200),
+            HeaderType::Dyn => ("Shared object file", 0x0300),
+            HeaderType::Core => ("Core file", 0x0400),
         };
         let data_vec: Vec<u8> = byte.to_ne_bytes().to_vec();
         display::print_field(f, "TYPE", data_str, &data_vec)
@@ -250,51 +242,73 @@ impl fmt::Display for HeaderType {
 
 // TODO: extend num list of all possible machines
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
-#[deku(type = "u8")]
+#[deku(type = "u16", bytes = "2", endian = "little")]
 pub enum HeaderMachine {
-    #[deku(id = "0x00")]
+    #[deku(id = "0x0000")]
     None,
-    #[deku(id = "0x01")]
+    #[deku(id = "0x0001")]
     WE32100,
-    #[deku(id = "0x02")]
+    #[deku(id = "0x0002")]
     Sparc,
-    #[deku(id = "0x03")]
+    #[deku(id = "0x0003")]
     X86,
-    #[deku(id = "0x04")]
+    #[deku(id = "0x0004")]
     Motorola68000,
-    #[deku(id = "0x05")]
+    #[deku(id = "0x0005")]
     Motorola88000,
-    #[deku(id = "0x06")]
+    #[deku(id = "0x0006")]
     IntelMCU,
-    #[deku(id = "0x07")]
+    #[deku(id = "0x0007")]
     Intel80860,
-    #[deku(id = "0x3E")]
+    #[deku(id = "0x003E")]
     X86_64,
 }
 
 impl fmt::Display for HeaderMachine {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let (data_str, byte): (&str, u8) = match *self {
-            HeaderMachine::None => ("unspecified instruction set", 0x00),
-            HeaderMachine::WE32100 => ("AT&T WE 32100", 0x01),
-            HeaderMachine::Sparc => ("SUN Sparc", 0x02),
-            HeaderMachine::X86 => ("x86", 0x03),
-            HeaderMachine::Motorola68000 => ("Motorola 68000 (m68k)", 0x04),
-            HeaderMachine::Motorola88000 => ("Motorola 88000 (m88k)", 0x05),
-            HeaderMachine::IntelMCU => ("Intel MCU", 0x06),
-            HeaderMachine::Intel80860 => ("Intel 80860", 0x07),
-            HeaderMachine::X86_64 => ("AMD x86-64", 0x3E),
+        let (data_str, byte): (&str, u16) = match *self {
+            HeaderMachine::None => ("unspecified instruction set", 0x0000),
+            HeaderMachine::WE32100 => ("AT&T WE 32100", 0x0100),
+            HeaderMachine::Sparc => ("SUN Sparc", 0x0200),
+            HeaderMachine::X86 => ("x86", 0x0300),
+            HeaderMachine::Motorola68000 => ("Motorola 68000 (m68k)", 0x0400),
+            HeaderMachine::Motorola88000 => ("Motorola 88000 (m88k)", 0x0500),
+            HeaderMachine::IntelMCU => ("Intel MCU", 0x0600),
+            HeaderMachine::Intel80860 => ("Intel 80860", 0x0700),
+            HeaderMachine::X86_64 => ("AMD x86-64", 0x003E),
         };
         let data_vec: Vec<u8> = byte.to_ne_bytes().to_vec();
         display::print_field(f, "MACHINE", data_str, &data_vec)
     }
 }
 
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku()]
+pub struct HeaderVersion {
+    #[deku(bytes = "1", count = "4")]
+    ei_version: Vec<u8>,
+}
+
+impl fmt::Display for HeaderVersion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        display::print_field(f, "VERSION", "version", &self.ei_version)
+    }
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku()]
+pub struct FileHeader {
+    ident: FileHeaderIdent,
+    r#type: HeaderType,
+    machine: HeaderMachine,
+    //    version: HeaderVersion,
+}
+
 impl fmt::Display for FileHeader {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "----------------   HEADER   ---\n{}\n{}\n{}",
+            "----------------   HEADER   ---\n{}\n{}\n{}\n",
             self.ident, self.r#type, self.machine,
         )
     }
@@ -337,9 +351,10 @@ pub fn parse_bytes_header_ident(data: &[u8]) -> Result<FileHeaderIdent, error::E
 }
 
 pub fn parse_bytes(data: &[u8]) -> Result<ElfFile, error::Error> {
+    debug!("len: {}", data.len());
     match ElfFile::from_bytes((data, 0)) {
         Ok((_rest, val)) => Ok(val),
-        Err(_deku_err) => {
+        Err(deku_err) => {
             /*
             let ident = match parse_bytes_header_ident(data) {
                 Ok(val) => val,
@@ -359,15 +374,50 @@ pub fn parse_bytes(data: &[u8]) -> Result<ElfFile, error::Error> {
                 }
             };
             */
+            println!("{}", deku_err);
+            error!("{}", deku_err);
             warn!("Incomplete ELF file. Trying to parse elf header ident ...");
             let ident = parse_bytes_header_ident(data)?;
 
-            let header_type = HeaderType::None;
-            let header_machine = HeaderMachine::None;
+            //let rest_type;
+            let mut header_machine: crate::elf::HeaderMachine = HeaderMachine::None;
+            let mut header_type: crate::elf::HeaderType = HeaderType::None;
+            // match HeaderType::from_bytes((&data[16..][..18], 0)) {
+            if data.len() >= 20 {
+                debug!("Bytes HeaderMachine - {:?}", &data[18..][..2]);
+                // header_type = match HeaderType::from_bytes((&data[16..][..2], 0)) {
+                header_machine = match HeaderMachine::from_bytes((&data[18..][..2], 0)) {
+                    Ok((_rest_type, val_type)) => {
+                        // header_type = val_type;
+                        val_type
+                    }
+                    Err(type_err) => {
+                        println!("{}", type_err);
+                        error!("{}", type_err);
+                        HeaderMachine::None
+                    }
+                };
+            }
+            if data.len() >= 18 {
+                println!("Bytes HeaderType - {:?}", &data[16..][..2]);
+                header_type = match HeaderType::from_bytes((&data[16..][..2], 0)) {
+                    Ok((_rest_type, val_type)) => {
+                        // header_type = val_type;
+                        val_type
+                    }
+                    Err(type_err) => {
+                        println!("{}", type_err);
+                        error!("{}", type_err);
+                        HeaderType::None
+                    }
+                };
+            }
+
             let header = FileHeader {
                 ident,
                 r#type: header_type,
                 machine: header_machine,
+                //                version: header_version,
             };
             let elffile = ElfFile {
                 file_header: header,
@@ -384,8 +434,8 @@ mod tests {
     #[test]
     fn test_parse_bytes() {
         let data: Vec<u8> = vec![
-            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05,
-            0x06, 0x07, 0x03, 0x3E,
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17, 0x03, 0x00, 0x3E, 0x00,
         ];
         let val = parse_bytes(&data).unwrap();
 
@@ -403,7 +453,7 @@ mod tests {
             ei_abiversion: 0x01,
         };
         let header_ident_padding = HeaderIdentPadding {
-            ei_pad: vec![0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07],
+            ei_pad: vec![0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17],
         };
         let ident = FileHeaderIdent {
             magic: header_ident_magic,
@@ -416,10 +466,14 @@ mod tests {
         };
         let header_type = HeaderType::Dyn;
         let header_machine = HeaderMachine::X86_64;
+        let header_version = HeaderVersion {
+            ei_version: vec![0x00, 0x01, 0x00, 0x00],
+        };
         let header = FileHeader {
             ident: ident,
             r#type: header_type,
             machine: header_machine,
+            //            version: header_version,
         };
         let expected = ElfFile {
             file_header: header,
@@ -506,10 +560,14 @@ mod tests {
         };
         let header_type = HeaderType::None;
         let header_machine = HeaderMachine::None;
+        let header_version = HeaderVersion {
+            ei_version: vec![0x01, 0x01, 0x01, 0x01],
+        };
         let header = FileHeader {
             ident: ident,
             r#type: header_type,
             machine: header_machine,
+            //            version: header_version,
         };
         let expected = ElfFile {
             file_header: header,
@@ -682,9 +740,10 @@ mod tests {
     #[test]
     fn test_parse_bytes_incomplete_type() {
         let data: Vec<u8> = vec![
-            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05,
-            0x06, 0x07,
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17,
         ];
+        println!("len orig: {}", data.len());
         let val = parse_bytes(&data).unwrap();
         let val_type = val.file_header.r#type;
         let expected = HeaderType::None;
@@ -692,14 +751,135 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_header_invalid_machine() {
+        let data: Vec<u8> = vec![
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17, 0x03, 0x00, 0x00, 0x00,
+        ];
+        println!("len orig: {}", data.len());
+        let val = parse_bytes(&data).unwrap();
+
+        let header_ident_magic = HeaderIdentMagic {
+            ei_mag0: 0x7F,
+            ei_mag1: 0x45,
+            ei_mag2: 0x4C,
+            ei_mag3: 0x46,
+        };
+        let header_ident_class = HeaderIdentClass::Bit32;
+        let header_ident_endian = HeaderIdentEndian::Little;
+        let header_ident_version = HeaderIdentVersion { ei_version: 0x01 };
+        let header_ident_osabi = HeaderIdentOsAbi::Linux;
+        let header_ident_abiversion = HeaderIdentAbiVersion {
+            ei_abiversion: 0x01,
+        };
+        let header_ident_padding = HeaderIdentPadding {
+            ei_pad: vec![0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17],
+        };
+        let ident = FileHeaderIdent {
+            magic: header_ident_magic,
+            class: header_ident_class,
+            endian: header_ident_endian,
+            version: header_ident_version,
+            osabi: header_ident_osabi,
+            abiversion: header_ident_abiversion,
+            padding: header_ident_padding,
+        };
+        let header_type = HeaderType::Dyn;
+        let header_machine = HeaderMachine::None;
+        let header_version = HeaderVersion {
+            ei_version: vec![0x00, 0x01, 0x00, 0x00],
+        };
+        let header = FileHeader {
+            ident: ident,
+            r#type: header_type,
+            machine: header_machine,
+            //            version: header_version,
+        };
+        let expected = ElfFile {
+            file_header: header,
+        };
+        println!("{}", &val);
+        assert_eq!(expected, val);
+
+        let data_out = val.to_bytes().unwrap();
+        assert_eq!(data, data_out);
+    }
+
+    #[test]
     fn test_parse_bytes_incomplete_machine() {
         let data: Vec<u8> = vec![
-            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05,
-            0x06, 0x07, 0x03,
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17, 0x03, 0x00,
         ];
         let val = parse_bytes(&data).unwrap();
-        let val_type = val.file_header.machine;
-        let expected = HeaderMachine::None;
-        assert_eq!(expected, val_type);
+        let val_type = val.file_header.r#type;
+        let val_machine = val.file_header.machine;
+        let expected_type = HeaderType::Dyn;
+        let expected_machine = HeaderMachine::None;
+        assert_eq!(expected_type, val_type);
+        assert_eq!(expected_machine, val_machine);
+    }
+
+    #[test]
+    fn test_parse_bytes_machine_direct() {
+        let data: Vec<u8> = vec![
+            0x7F, 0x45, 0x4C, 0x46, 0x01, 0x01, 0x01, 0x03, 0x01, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17, 0x03, 0x00, 0x3E, 0x00,
+        ];
+        match ElfFile::from_bytes((&data, 0)) {
+            Ok((rest, val)) => {
+                println!("{:?}", rest);
+                println!("{}", val);
+                let val_type = val.file_header.r#type;
+                let val_machine = val.file_header.machine;
+                let expected_type = HeaderType::Dyn;
+                let expected_machine = HeaderMachine::X86_64;
+                assert_eq!(expected_type, val_type);
+                assert_eq!(expected_machine, val_machine);
+            }
+            Err(err) => {
+                /*
+                let ident = match parse_bytes_header_ident(data) {
+                    Ok(val) => val,
+                    Err(err) => {
+                        Err(err)
+                      /*
+                        let error_affected = String::from("unknown");
+                        let error_suggestion = String::from("verify the file format");
+                        let error_type = error::ErrorType::Deku(deku_err);
+                        let err = error::Error {
+                            error_type,
+                            affected: error_affected,
+                            suggestion: error_suggestion,
+                        };
+                        Err(err)
+                      */
+                    }
+                };
+                */
+                println!("{:?}", err);
+                error!("{}", err);
+                warn!("Incomplete ELF file. Trying to parse elf header ident ...");
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_header_type() {
+        let data: Vec<u8> = vec![0x01, 0x00];
+        let (_rest, val) = HeaderType::from_bytes((&data, 0)).unwrap();
+        let expected = HeaderType::Rel;
+        println!("{:?}", val);
+        assert_eq!(expected, val);
+    }
+
+    #[test]
+    fn test_parse_header_machine() {
+        let data: Vec<u8> = vec![0x3E, 0x00];
+        let (_rest, val) = HeaderMachine::from_bytes((&data, 0)).unwrap();
+        let expected = HeaderMachine::X86_64;
+        println!("{:?}", val);
+        assert_eq!(expected, val);
     }
 }
