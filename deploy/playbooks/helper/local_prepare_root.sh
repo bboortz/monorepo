9a134cd722120725bbc1811bcbc7d892fda82086
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2022-01-23 21:43:17 +0100

    update

--- deploy/playbooks/helper/local_prepare_root.sh
+++ deploy/playbooks/helper/local_prepare_root.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 source /etc/os-release
+INSTALL_CMD="apt-get install"
+if [ "$ID_LIKE" == "arch" ]; then
+	INSTALL_CMD="pacman -S"
+fi
 
+echo "INFO: installing necessary OS packages ..."
 
-which python3 || {
-  echo "INFO: installing necessary OS packages"
-  if [ "$ID_LIKE" == "arch" ]; then
-    sudo pacman -S make python3 python-pip
-  else
-    apt-get install git make python3 python3-venv python3-pip
-  fi
-}
+which make || $INSTALL_CMD make
+which python3 || $INSTALL_CMD python3
+if [ "$ID_LIKE" == "arch" ]; then
+	which python-pip || $INSTALL_CMD python-pip
+else
+	which python3-venv || $INSTALL_CMD python3-venv
+	which python3-pip || $INSTALL_CMD python3-pip
+fi
