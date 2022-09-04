d50925e987c2370344c60ca848d352cd4610923f
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2022-09-04 16:36:07 +0200

    bookmarks updated

--- benni.tech/content/check_links.sh
+++ benni.tech/content/check_links.sh
@@ -6,18 +6,35 @@ set -u
 USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0"
 err_count=0
 
-while read f; do
-  echo -ne "** $f  --> "
-  resp_code=$( curl -o /dev/null --no-progress-meter -w "%{http_code}\n" -L $f || echo $?)
+check_link() {
+  local f="$1"
+
+  # echo -ne "** $f  --> "
+  resp_code=$( curl -o /dev/null --no-progress-meter -w "%{http_code}\n" -L --connect-timeout 3 --max-time 10 $f || echo $?)
   # resp_code=$( curl -o /dev/null --no-progress-meter -w "%{http_code}\n" -A "{$USER_AGENT}" $f || echo $?)
 
   if [ "$resp_code" != "200" ]; then
       let "err_count = err_count + 1"
-      echo " *** ERROR: $resp_code ***"
+      echo "** $f  -->  *** ERROR: $resp_code ***"
+      #echo " *** ERROR: $resp_code ***"
   else
-      echo " OK"
+      echo "** $f  -->  OK"
+      # echo " OK"
   fi
+}
+
+while read f; do
+  check_link "$f" &
+  sleep 0.1
 done < <( cat bookmarks.md| gawk 'match($0, /^* \[(.*)\]\((.*)\)/, a) {print a[2]}')
+
+
+while true; do
+  if [ $(jobs | grep Running | wc -l) -eq 0 ]; then
+    break 
+  fi
+  sleep 1
+done
 
 echo "NUMBER_OF_ERRORS: $err_count"
 exit $err_count
